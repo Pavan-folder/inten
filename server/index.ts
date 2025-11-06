5a5a243dc416862ae4c1a5353e070ca0da8ad977
Pavan <seethipavankumarreddy@email.com>
2025-11-06 15:22:10 +0530

    Initial fullstack commit

--- server/index.ts
+++ server/index.ts
@@ -3,6 +3,7 @@ import { registerRoutes } from "./routes";
 import { setupVite, serveStatic, log } from "./vite";
 
 const app = express();
+console.log('NODE_ENV:', process.env.NODE_ENV);
 
 declare module 'http' {
   interface IncomingMessage {
@@ -71,11 +72,7 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-  server.listen({
-    port,
-    host: "0.0.0.0",
-    reusePort: true,
-  }, () => {
+  server.listen(port, () => {
     log(`serving on port ${port}`);
   });
 })();
